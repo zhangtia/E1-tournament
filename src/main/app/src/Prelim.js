85866ab6d80fffa006d9d245c9108259ad31738b
zhangtia <zhangtia@umich.edu>
2019-12-02 16:35:20 -0500

    .

--- src/main/app/src/Prelim.js
+++ src/main/app/src/Prelim.js
@@ -3,7 +3,7 @@ import { Button, Table, Container, Form, FormGroup, Input, Label } from 'reactst
 import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
 
-class Prelim extends Component {
+/*class Prelim extends Component {
 
     constructor(props) {
         super(props);
@@ -57,6 +57,96 @@ class Prelim extends Component {
                 <AppNavbar />
                 <Container fluid>
                     <div className="float-right">
+                        <Button color="danger" onClick={() => this.removeall()}>Delete All</Button>
+                        <Button color="success" tag={Link} to="/groups/new">Add Competitor</Button>
+                    </div>
+                    <h3>Competitors</h3>
+                    <Table className="mt-4">
+                        <thead>
+                            <tr>
+                                <th width="20%">Name</th>
+                                <th width="20%">Crew</th>
+                                <th>Score</th>
+                                <th width="10%">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {groupList}
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+}*/
+class Prelim extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { groups: [], isLoading: true };
+        this.remove = this.remove.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+
+        fetch('api/groups')
+            .then(response => response.json())
+            .then(data => this.setState({ groups: data, isLoading: false }));
+    }
+
+    async remove(id) {
+        await fetch(`/api/group/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then(() => {
+            let updatedGroups = [...this.state.groups].filter(i => i.id !== id);
+            this.setState({ groups: updatedGroups });
+        });
+    }
+
+    async removeall() {
+        await fetch(`/api/group`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        fetch('api/groups')
+            .then(response => response.json())
+            .then(data => this.setState({ groups: data, isLoading: false }));
+    }
+
+    render() {
+        const { groups, isLoading } = this.state;
+
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+
+        const groupList = groups.map(group => {
+            return <tr key={group.id}>
+                <td style={{ whiteSpace: 'nowrap' }}>{group.name}</td>
+                <td>{group.address}</td>
+                <td>{group.score}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary" tag={Link} to={"/groups/" + group.id}>Edit</Button>
+                        <Button size="sm" color="danger" onClick={() => this.remove(group.id)}>Delete</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        });
+
+        return (
+            <div>
+                <AppNavbar />
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="primary" tag={Link} to={"/prelim"}>THIS IS PRELIM IT WORKED</Button>
                         <Button color="danger" onClick={() => this.removeall()}>Delete All</Button>
                         <Button color="success" tag={Link} to="/groups/new">Add Competitor</Button>
                     </div>
