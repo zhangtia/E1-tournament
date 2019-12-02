081c6bde38408120e607effa007865efa2151a3d
zhangtia <zhangtia@umich.edu>
2019-12-02 18:43:26 -0500

    adding prelim scores

--- src/main/app/src/Prelim.js
+++ src/main/app/src/Prelim.js
@@ -51,7 +51,7 @@ class Prelim extends Component {
                 <td>
                     <InputGroup>
                     <Input type="double" name="score" id="score" value={group.score || ''}/>
-                        <InputGroupAddon addonType="append"><Button color="secondary" onClick={() => this.score}>Update Score</Button></InputGroupAddon>
+                        <InputGroupAddon addonType="append"><Button color="secondary">Update Score</Button></InputGroupAddon>
                     </InputGroup>
                 </td>
             </tr>
