9a78e0c0dce7fbf94e9239e2a1d8cf77ffd27394
zhangtia <zhangtia@umich.edu>
2019-12-02 17:08:06 -0500

    .

--- src/main/app/src/Prelim.js
+++ src/main/app/src/Prelim.js
@@ -3,82 +3,7 @@ import { Button, ButtonGroup, Table, Container, Form, FormGroup, Input, Label } 
 import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
 
-/*class Prelim extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { groups: [], isLoading: true };
-        this.remove = this.remove.bind(this);
-    }
-
-    componentDidMount() {
-        this.setState({ isLoading: true });
-
-        fetch('api/groups')
-            .then(response => response.json())
-            .then(data => this.setState({ groups: data, isLoading: false }));
-    }
-
-    async update(event) {
-        event.preventDefault();
-        const { item } = this.state;
-        await fetch(`/api/group/${item.id}`, {
-            method: 'PUT',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(item),
-        });
-
-        this.props.history.push('/groups');
-    }
-
-    render() {
-        const { groups, isLoading } = this.state;
-
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
-
-        const groupList = groups.map(group => {
-            return <tr key={group.id}>
-                <td style={{ whiteSpace: 'nowrap' }}>{group.name}</td>
-                <td>{group.address}</td>
-                <td>{group.score}</td>
-                <td>
-
-                </td>
-            </tr>
-        });
-
-        return (
-            <div>
-                <AppNavbar />
-                <Container fluid>
-                    <div className="float-right">
-                        <Button color="danger" onClick={() => this.removeall()}>Delete All</Button>
-                        <Button color="success" tag={Link} to="/groups/new">Add Competitor</Button>
-                    </div>
-                    <h3>Competitors</h3>
-                    <Table className="mt-4">
-                        <thead>
-                            <tr>
-                                <th width="20%">Name</th>
-                                <th width="20%">Crew</th>
-                                <th>Score</th>
-                                <th width="10%">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {groupList}
-                        </tbody>
-                    </Table>
-                </Container>
-            </div>
-        );
-    }
-}*/
 class Prelim extends Component {
     constructor(props) {
         super(props);
@@ -133,10 +58,16 @@ class Prelim extends Component {
                 <td>{group.address}</td>
                 <td>{group.score}</td>
                 <td>
-                    <ButtonGroup>
-                        <Button size="sm" color="primary" tag={Link} to={"/groups/" + group.id}>Edit</Button>
-                        <Button size="sm" color="danger" onClick={() => this.remove(group.id)}>Delete</Button>
-                    </ButtonGroup>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Control placeholder="Prelim Score" />
+                            </Col>
+                            <Col>
+                                <Button variant="primary" type="submit">Update Score</Button>
+                            </Col>
+                        </Row>
+                    </Form>
                 </td>
             </tr>
         });
@@ -147,8 +78,8 @@ class Prelim extends Component {
                 <Container fluid>
                     <div className="float-right">
                         <Button color="primary" tag={Link} to={"/prelim"}>THIS IS PRELIM IT WORKED</Button>
-                        <Button color="danger" onClick={() => this.removeall()}>Delete All</Button>
-                        <Button color="success" tag={Link} to="/groups/new">Add Competitor</Button>
+                        <Button color="danger" onClick={() => this.removeall()}>REMOVE THE</Button>
+                        <Button color="success" tag={Link} to="/groups/new">SE BUTTONS</Button>
                     </div>
                     <h3>Competitors</h3>
                     <Table className="mt-4">
