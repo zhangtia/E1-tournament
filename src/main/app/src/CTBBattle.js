f99617f03e1608f67fa9f5ee5730dc167b6bcc69
zhangtia <zhangtia@umich.edu>
2019-12-07 18:31:55 -0500

    test color change

--- src/main/app/src/CTBBattle.js
+++ src/main/app/src/CTBBattle.js
@@ -21,7 +21,8 @@ class CTBBattle extends Component {
         C1: '',
         C2: '',
     }
-    onChange = () => {
+    onChange = (id) => {
+        alert(id);
         this.setState({ color: 'green' });
     }
 
@@ -31,7 +32,7 @@ class CTBBattle extends Component {
                 <AppNavbar />
                 <Container fluid>
                     <Row>
-                        <Col><Button color="primary">A1</Button></Col>
+                        <Col><Button color="primary" id="first" onClick={() => this.onChange(this.id)}>A1</Button></Col>
                         <Col>&nbsp;</Col>
                         <Col>&nbsp;</Col>
                         <Col>&nbsp;</Col>
@@ -132,6 +133,177 @@ class CTBBattle extends Component {
                         <Col>14</Col>
                         <Col>15</Col>
                     </Row>
+                    <Row>
+                        <Col>1</Col>
+                        <Col>2</Col>
+                        <Col>3</Col>
+                        <Col>4</Col>
+                        <Col><Button color="primary">C1</Button></Col>
+                        <Col>6</Col>
+                        <Col>7</Col>
+                        <Col>8</Col>
+                        <Col>9</Col>
+                        <Col>10</Col>
+                        <Col><Button color="primary">C2</Button></Col>
+                        <Col>12</Col>
+                        <Col>13</Col>
+                        <Col>14</Col>
+                        <Col>15</Col>
+                    </Row>
+                    <Row>
+                        <Col><Button color="primary">A2</Button></Col>
+                        <Col>2</Col>
+                        <Col>3</Col>
+                        <Col>4</Col>
+                        <Col><Button color="primary">C1</Button></Col>
+                        <Col>6</Col>
+                        <Col>7</Col>
+                        <Col>8</Col>
+                        <Col>9</Col>
+                        <Col>10</Col>
+                        <Col><Button color="primary">C2</Button></Col>
+                        <Col>12</Col>
+                        <Col>13</Col>
+                        <Col>14</Col>
+                        <Col><Button color="primary">A6</Button></Col>
+                    </Row>
+                    <Row>
+                        <Col><Button color="primary">A2</Button></Col>
+                        <Col>2</Col>
+                        <Col><Button color="primary">B1</Button></Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col>7</Col>
+                        <Col>8</Col>
+                        <Col>9</Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col><Button color="primary">B3</Button></Col>
+                        <Col>14</Col>
+                        <Col><Button color="primary">A6</Button></Col>
+                    </Row>
+                    <Row>
+                        <Col>1</Col>
+                        <Col>2</Col>
+                        <Col><Button color="primary">B1</Button></Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col>7</Col>
+                        <Col>8</Col>
+                        <Col>9</Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col><Button color="primary">B3</Button></Col>
+                        <Col>14</Col>
+                        <Col>15</Col>
+                    </Row>
+                    <Row>
+                        <Col><Button color="primary">A2</Button></Col>
+                        <Col>2</Col>
+                        <Col><Button color="primary">B1</Button></Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col>7</Col>
+                        <Col>8</Col>
+                        <Col>9</Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col><Button color="primary">B3</Button></Col>
+                        <Col>14</Col>
+                        <Col><Button color="primary">A6</Button></Col>
+                    </Row>
+                    <Row>
+                        <Col><Button color="primary">A2</Button></Col>
+                        <Col>2</Col>
+                        <Col>3</Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col><Button color="primary">F</Button></Col>
+                        <Col>8</Col>
+                        <Col><Button color="primary">F</Button></Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col>13</Col>
+                        <Col>14</Col>
+                        <Col><Button color="primary">A6</Button></Col>
+                    </Row>
+                    <Row>
+                        <Col>1</Col>
+                        <Col>2</Col>
+                        <Col>3</Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col><Button color="primary">I</Button></Col>
+                        <Col>8</Col>
+                        <Col><Button color="primary">I</Button></Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col>13</Col>
+                        <Col>14</Col>
+                        <Col>15</Col>
+                    </Row>
+                    <Row>
+                        <Col>1</Col>
+                        <Col>2</Col>
+                        <Col>3</Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col><Button color="primary">N</Button></Col>
+                        <Col>8</Col>
+                        <Col><Button color="primary">N</Button></Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col>13</Col>
+                        <Col>14</Col>
+                        <Col>15</Col>
+                    </Row>
+                    <Row>
+                        <Col>1</Col>
+                        <Col>2</Col>
+                        <Col>3</Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col><Button color="primary">A</Button></Col>
+                        <Col>8</Col>
+                        <Col><Button color="primary">A</Button></Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col>13</Col>
+                        <Col>14</Col>
+                        <Col>15</Col>
+                    </Row>
+                    <Row>
+                        <Col><Button color="primary">A3</Button></Col>
+                        <Col>2</Col>
+                        <Col>3</Col>
+                        <Col>4</Col>
+                        <Col>5</Col>
+                        <Col>6</Col>
+                        <Col><Button color="primary">L</Button></Col>
+                        <Col>8</Col>
+                        <Col><Button color="primary">L</Button></Col>
+                        <Col>10</Col>
+                        <Col>11</Col>
+                        <Col>12</Col>
+                        <Col>13</Col>
+                        <Col>14</Col>
+                        <Col><Button color="primary">A7</Button></Col>
+                    </Row>
+
                     <Row><div style={{ backgroundColor: this.state.color }}>please change</div></Row>
                 </Container>
             </div>
