e5383fc3725f26bfc1498106bd241a3029e0ef1d
zhangtia <zhangtia@umich.edu>
2019-12-08 21:06:09 -0500

    bracket const col width

--- src/main/app/src/CTB.js
+++ src/main/app/src/CTB.js
@@ -34,6 +34,23 @@ class CTBBattle extends Component {
                 <AppNavbar />
                 <div class="wrapper">
                     <div class="box a11">A11</div>
+                    <div class="box a12">A12</div>
+                    <div class="box a21">A21</div>
+                    <div class="box a22">A22</div>
+                    <div class="box a31">A31</div>
+                    <div class="box a32">A32</div>
+                    <div class="box a41">A41</div>
+                    <div class="box a42">A42</div>
+                    <div class="box a51">A51</div>
+                    <div class="box a52">A52</div>
+                    <div class="box a61">A61</div>
+                    <div class="box a62">A62</div>
+                    <div class="box a71">A71</div>
+                    <div class="box a72">A72</div>
+                    <div class="box a81">A81</div>
+                    <div class="box a82">A82</div>
+                    
+                    
                     <div class="box b12">B12</div>
                     <div class="box c12">C12</div>
                     <div class="box a12">A12</div>
