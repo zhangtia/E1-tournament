090b352a51b2d6369033f70b7cb5f09b4b142016
zhangtia <zhangtia@umich.edu>
2019-12-09 01:05:31 -0500

    buttons

--- src/main/app/src/CTB.js
+++ src/main/app/src/CTB.js
@@ -29,7 +29,7 @@ class CTBBattle extends Component {
     }
 
     setA1 = (x) => {
-        if (this.state.A1 === x) {
+        if (this.state.A1 !== '' ) {
             this.setState({ A2: '' });
         }
         this.setState({ A1: x })
@@ -40,8 +40,8 @@ class CTBBattle extends Component {
             <div>
                 <AppNavbar />
                 <div class="wrapper">
-                    <div class="box a11"><Button color="primary"  disabled={this.state.A1} onClick={() => this.setA1('1')}>A11</Button></div>
-                    <div class="box a12"><Button color="primary"  disabled={this.state.A1} onClick={() => this.setA1('1')}>A12</Button></div>
+                    <div class="box a11"><Button color="primary"  disabled={this.state.A1 && this.state.A1 == '1'} onClick={() => this.setA1('1')}>A11</Button></div>
+                    <div class="box a12"><Button color="primary"  disabled={this.state.A1 && this.state.A1 == '1'} onClick={() => this.setA1('1')}>A12</Button></div>
                     <div class="box a21"><Button color="primary"  disabled={this.state.A2} onClick={() => this.setA1('')}>A21</Button></div>
                     <div class="box a22"><Button color="primary"  disabled={this.state.A2} onClick={() => this.setA1('')}>A22</Button></div>
                     <div class="box a31">A31</div>
