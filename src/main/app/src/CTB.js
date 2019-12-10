f6a59ff53bd1771e5ea57558bb387bc7eba18302
zhangtia <zhangtia@umich.edu>
2019-12-10 16:17:09 -0500

    work pls

--- src/main/app/src/CTB.js
+++ src/main/app/src/CTB.js
@@ -175,8 +175,8 @@ class CTBBattle extends Component {
                     <div class="box a81" id="a81"><Button color="primary" disabled={this.state.A7C === '#' || this.state.A7C === '' || this.state.A8 === '1' || this.state.A8C === '1' || this.state.A8C === '2'} onClick={() => this.setA8('1')}>A81</Button></div>
                     <div class="box a82" id="a82"><Button color="primary" disabled={this.state.A7C === '#' || this.state.A7C === '' || this.state.A8 === '2' || this.state.A8C === '1' || this.state.A8C === '2'} onClick={() => this.setA8('2')}>A82</Button></div>
 
-                    <div class="box b11">B11</div>
-                    <div class="box b12">B12</div>
+                    <div class="box b11"><Button color="primary" disabled={this.state.A8C === '#' || this.state.A8C === '' || this.state.B1 === '1' || this.state.B1C === '1' || this.state.B1C === '2'} onClick={() => this.setB1('1')}>B11</Button></div>
+                    <div class="box b12"><Button color="primary" disabled={this.state.A8C === '#' || this.state.A8C === '' || this.state.B1 === '2' || this.state.B1C === '1' || this.state.B1C === '2'} onClick={() => this.setB1('2')}>B12</Button></div>
                     <div class="box b21">B21</div>
                     <div class="box b22">B22</div>
                     <div class="box b31">B31</div>
@@ -264,6 +264,7 @@ class CTBBattle extends Component {
                             <Button onClick={() => this.setA8C('2')}>{(this.state.A8 === '2') ? "A1 Second" : "A2 Second"}</Button>
                         </PopoverBody>
                     </UncontrolledPopover>
+
 
                 </div>
 
