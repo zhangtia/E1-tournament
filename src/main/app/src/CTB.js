9857aaab6f676a628ed8f5b2089cf5f6bbed96aa
zhangtia <zhangtia@umich.edu>
2019-12-11 21:24:38 -0500

    work pls

--- src/main/app/src/CTB.js
+++ src/main/app/src/CTB.js
@@ -238,13 +238,13 @@ class CTBBattle extends Component {
             arr.push(x.name);
         });
 
-        alert(hi);
+        //alert(hi);
 
         return (
             <div>
                 <AppNavbar />
                 <div class="wrapper">
-                    <div class="box a11" id="a11" disabled={this.state.A1 === '1' || this.state.A1C === '1' || this.state.A1C === '2'} onClick={() => this.setA1('1')}>{arr[0]}</div>
+                    <Button id="a11" color="primary" disabled={this.state.A1 === '1' || this.state.A1C === '1' || this.state.A1C === '2'} onClick={() => this.setA1('1')}><div>{arr[0]}</div></Button>
                     <div class="box a12" id="a12"><Button color="primary" disabled={this.state.A1 === '2' || this.state.A1C === '1' || this.state.A1C === '2'} onClick={() => this.setA1('2')}>{arr[1]}</Button></div>
                     <div class="box a21" id="a21"><Button color="primary" disabled={this.state.A1C === '#' || this.state.A1C === '' || this.state.A2 === '1' || this.state.A2C === '1' || this.state.A2C === '2'} onClick={() => this.setA2('1')}>A21</Button></div>
                     <div class="box a22" id="a22"><Button color="primary" disabled={this.state.A1C === '#' || this.state.A1C === '' || this.state.A2 === '2' || this.state.A2C === '1' || this.state.A2C === '2'} onClick={() => this.setA2('2')}>A22</Button></div>
