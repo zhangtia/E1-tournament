926849aa8a30f4b70de51fa46ae546d67b2c9e5d
zhangtia <zhangtia@umich.edu>
2019-12-21 12:50:40 -0500

    learning about props and state

--- src/main/app/src/CTBBattle.js
+++ src/main/app/src/CTBBattle.js
@@ -76,8 +76,8 @@ class CTBBattle extends Component {
 
     leftwin() {
         alert("pls no");
-        this.setState(state => ({ isShow: true }));
-        //alert(this.state.isShow);
+        this.setState({ isShow: true });
+        alert("yes pls");
 
     }
 
@@ -113,7 +113,7 @@ class CTBBattle extends Component {
                 <AppNavbar />
                 <EnhancedTable parentCallback={this.leftwin} />         
                 <div class="wrapper">
-                    <div class="box a11" id="a11"><Button onClick={() => alert(this.state.isShow)} style={{ height: "100%", width: "100%" }} color="primary" disabled={this.state.A1 === '1' || this.state.A1C === '1' || this.state.A1C === '2'} onClick={() => this.setA1('1')}><p>smth</p></Button></div>
+                    <div class="box a11" id="a11"><Button onClick={() => alert(this.state.isShow)} style={{ height: "100%", width: "100%" }} color="primary" disabled={this.state.A1 === '1' || this.state.A1C === '1' || this.state.A1C === '2'}><p>smth</p></Button></div>
                     <div class="box a12" id="a12"><Button onClick={() => alert(this.state.battle)} style={{ height: "100%", width: "100%" }} color="primary" disabled={this.state.A1 === '2' || this.state.A1C === '1' || this.state.A1C === '2'} onClick={() => this.setA1('2')}>smth</Button></div>
                     <div class="box a21" id="a21"><Button style={{ height: "100%", width: "100%" }} color="primary" disabled={this.state.A1C === '#' || this.state.A1C === '' || this.state.A2 === '1' || this.state.A2C === '1' || this.state.A2C === '2'} onClick={() => this.setA2('1')}>A21</Button></div>
                     <div class="box a22" id="a22"><Button style={{ height: "100%", width: "100%" }} color="primary" disabled={this.state.A1C === '#' || this.state.A1C === '' || this.state.A2 === '2' || this.state.A2C === '1' || this.state.A2C === '2'} onClick={() => this.setA2('2')}>A22</Button></div>
