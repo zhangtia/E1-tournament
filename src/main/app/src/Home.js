e6a7ecc5f2445f746a3e7b554c189f2b458b187c
zhangtia <zhangtia@umich.edu>
2019-12-22 23:52:39 -0500

    learning about props and state

--- src/main/app/src/Home.js
+++ src/main/app/src/Home.js
@@ -3,62 +3,35 @@ import './App.css';
 import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { Button, Container, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-
-const ModalExample = ({ fnct, anoth }) => {
-  /*return (
-    <Button onClick={fnct} type="button">
-      Toggle Show
-  </Button>
-  );
+import { TabContent, TabPane, Nav, NavItem, NavLink, Container, Button, Row, Col } from 'reactstrap';
 
 
-
-  const {
-    buttonLabel,
-    className
-  } = props;*/
-
-
-  const [modal, setModal] = useState(false);
-
-  const toggle = () => setModal(!modal);
-
-  return (
-    <div>
-      <Button color="primary" onClick={anoth}>Do Something</Button>
-      <Button color="danger" onClick={toggle}>open</Button>
-      <Modal isOpen={modal} toggle={toggle} className="its 4am">
-        <ModalHeader toggle={toggle}>header</ModalHeader>
-        <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={fnct}>Do Something</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
-        </ModalFooter>
-      </Modal>
-    </div>
-  );
-}
+<Tab.Container id="left-tabs-example" defaultActiveKey="first">
+  <Row>
+    <Col sm={3}>
+      <Nav variant="pills" className="flex-column">
+        <Nav.Item>
+          <Nav.Link eventKey="first">Tab 1</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="second">Tab 2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Col>
+    <Col sm={9}>
+      <Tab.Content>
+        <Tab.Pane eventKey="first">
+          <Sonnet />
+        </Tab.Pane>
+        <Tab.Pane eventKey="second">
+          <Sonnet />
+        </Tab.Pane>
+      </Tab.Content>
+    </Col>
+  </Row>
+</Tab.Container>
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShow: "PREVIOUS",
-    };
-  }
-
-  fnc = () => {
-    //alert("fnc!");
-    this.setState(state => ({ isShow: "NEWWWWWWWW" }));
-  }
-
-  anotha = () => {
-    //alert("fnc!");
-    this.setState(state => ({ isShow: "ANOTHA ONE" }));
-  }
 
   render() {
     return (
