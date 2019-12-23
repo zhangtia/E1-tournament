3366760633fd5f101a8453167d365ddf7c233bf7
zhangtia <zhangtia@umich.edu>
2019-12-22 23:58:06 -0500

    learning about props and state

--- src/main/app/src/Home.js
+++ src/main/app/src/Home.js
@@ -3,33 +3,38 @@ import './App.css';
 import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Container, Button, Row, Col } from 'reactstrap';
+import { TabContent, Tab, Nav, NavItem, NavLink, Container, Button, Row, Col } from 'reactstrap';
 
-
-<Tab.Container id="left-tabs-example" defaultActiveKey="first">
-  <Row>
-    <Col sm={3}>
-      <Nav variant="pills" className="flex-column">
-        <Nav.Item>
-          <Nav.Link eventKey="first">Tab 1</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="second">Tab 2</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Col>
-    <Col sm={9}>
-      <Tab.Content>
-        <Tab.Pane eventKey="first">
-          <Sonnet />
-        </Tab.Pane>
-        <Tab.Pane eventKey="second">
-          <Sonnet />
-        </Tab.Pane>
-      </Tab.Content>
-    </Col>
-  </Row>
-</Tab.Container>
+function ControlledTabs() {
+  const [key, setKey] = useState('home');
+  return (
+    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Row>
+        <Col sm={3}>
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Col>
+        <Col sm={9}>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
+              hi1
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              hi
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
+  );
+}
+render(<ControlledTabs />);
 
 class Home extends Component {
 
